4c2f449d94bd6f247ca39e01752db818454afe3f
jimmynXD <87709880+jimmynXD@users.noreply.github.com>
2022-10-18 07:45:49 -0400

    feat: link preview

--- apps/website/src/pages/_document.tsx
+++ apps/website/src/pages/_document.tsx
@@ -20,9 +20,33 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta
-            name="description"
-            content="Generate a survey and get meaningful visualizations."
+            name="keywords"
+            content="ux, dx, labxd, hausofxd, chatterxd, developer experience, user experience, TypeScript"
           />
+          <meta
+            name="description"
+            content="Home for experience development, user and developer focused"
+          />
+          <link rel="canonical" href="https://labd.com/" />
+
+          <meta property="og:title" content="labXD" />
+          <meta
+            property="og:description"
+            content="Home for experience development, user and developer focused"
+          />
+          <meta property="og:url" content="https://labxd.com" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="https://labxd.com/cover.png" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@hausofxd" />
+          <meta name="twitter:creator" content="@hausofxd" />
+          <meta name="twitter:title" content="Haus of XD" />
+          <meta
+            name="twitter:description"
+            content="Haus of XD is the voice for labXD, the home for experience development"
+          />
+          <meta name="twitter:image" content="https://labxd.com/cover.png" />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
